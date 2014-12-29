96c9157763d934769324d22eff4306a8d75fcb0a
Sebastian Eichelbaum <code@nemtics.com>
2014-12-29 18:57:59 +0100

    [ADD] initial program skeleton

---
+++ src/gui/main.cpp
@@ -0,0 +1,46 @@
+//---------------------------------------------------------------------------------------
+//
+// Project: DirectionalityIndicator
+//
+// Copyright 2014-2015 Sebastian Eichelbaum (http://www.sebastian-eichelbaum.de)
+//           2014-2015 Max Planck Research Group "Neuroanatomy and Connectivity"
+//
+// This file is part of DirectionalityIndicator.
+//
+// DirectionalityIndicator is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DirectionalityIndicator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DirectionalityIndicator. If not, see <http:#www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------------------
+
+#include <iostream>
+
+#include <QApplication>
+
+/**
+ * The main routine starting up the whole application.
+ */
+int main( int argc, char** argv )
+{
+    QApplication appl( argc, argv, true );
+    appl.setApplicationName( "DirectionalityIndicator" );
+    appl.setApplicationVersion( "1.0" );
+
+    // main window
+    std::cout << "Work In Progress" << std::endl;
+    return 0;
+
+    // run
+    // int result= appl.exec();
+    // return result;
+}
+
