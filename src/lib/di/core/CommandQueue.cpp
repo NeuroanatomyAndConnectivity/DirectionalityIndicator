NeuroanatomyAndConnectivity/DirectionalityIndicator

commit fc0de0d31bad94e7210890e5a2f17defbba4970c

[CHANGE] Algorithms now react on parameter changes and the algorithm network automatically updates.

src/lib/di/core/CommandQueue.cpp
@@ -108,6 +108,8 @@ namespace di
                         continue;
                     }
 
+                    // unlock as this would (otherwise) block the queue itself when "processCommand" is blocking
+                    m_commandQueueMutex.unlock();
                     // process
                     processCommand( command );
                 }

src/app/App.cpp
@@ -77,7 +77,7 @@ namespace di
         void App::onDirtyNetwork()
         {
             LogD << "Network marked dirty. Requesting update." << LogEnd;
-            // getProcessingNetwork()->runNetwork();
+            getProcessingNetwork()->runNetwork();
         }
 
         void App::prepareUI()

src/lib/di/Types.h
@@ -54,6 +54,20 @@ namespace di
     template< typename T > using ConstSPtr = std::shared_ptr< const T >;
 
     /**
+     * Alias for abbreviating the often used std::weak_ptr< Type >.
+     *
+     * \tparam T the type to embed into the weak_ptr.
+     */
+    template< typename T > using WPtr = std::weak_ptr< T >;
+
+    /**
+     * Alias for abbreviating the often used std::weak_ptr< Type >.
+     *
+     * \tparam T the type to embed into the weak_ptr.
+     */
+    template< typename T > using ConstWPtr = std::weak_ptr< const T >;
+
+    /**
      * Alias for abbreviating often used shared pointer vector containers
      *
      * \tparam T the type in the shared_ptr vector.

src/lib/di/algorithms/DataInject.cpp
@@ -58,7 +58,6 @@ namespace di
             }
             LogD << "Injecting data instance " << static_cast< const void* >( m_injectionData.get() ) << "." << LogEnd;
             m_dataOutput->setData( m_injectionData );
-            requestUpdate( false );
         }
 
         void DataInject::inject( ConstSPtr< di::core::ConnectorTransferable > data )

src/lib/di/core/Algorithm.cpp
@@ -26,6 +26,8 @@
 #include <string>
 
 #include <di/core/ObserverCallback.h>
+#include <di/core/ObserverParameter.h>
+
 #include "Algorithm.h"
 
 #include <di/core/Logger.h>
@@ -85,13 +87,21 @@ namespace di
 
         void Algorithm::addParameter( SPtr< ParameterBase > parameter )
         {
-            parameter->observe( std::make_shared< ObserverCallback >( [ this ]()
+            parameter->observe(
+                    std::make_shared< ObserverParameter >( [ & ]( SPtr< ParameterBase > param )
                     {
-                        this->requestUpdate();
-                    }
+                        this->onParameterChange( param );
+                    },
+                    parameter
                 )
             );
             m_parameters.insert( parameter );
+        }
+
+        void Algorithm::onParameterChange( SPtr< ParameterBase > parameter )
+        {
+            LogD << "Change in parameter: " << parameter->getName() << ". Requesting update." << LogEnd;
+            requestUpdate();
         }
 
         bool Algorithm::isInput( ConstSPtr< ConnectorBase > connector ) const
@@ -131,8 +141,9 @@ namespace di
 
         void Algorithm::requestUpdate( bool request )
         {
-            if( m_updateRequested == request )
+            if( ( m_updateRequested == request ) || ( !request ) )
             {
+                m_updateRequested = request;
                 return;
             }
             m_updateRequested = request;
@@ -176,6 +187,12 @@ namespace di
         {
             os << obj.getName() << std::string( " (" ) << static_cast< const void* >( &obj ) << std::string( ")" );
             return os;
+        }
+
+        void Algorithm::run()
+        {
+            process();
+            requestUpdate( false );
         }
     }
 }

src/lib/di/core/Algorithm.h
@@ -70,6 +70,11 @@ namespace di
             virtual void process() = 0;
 
             /**
+             * Run the algorithm in the calling thread. It also resets the updateRequest.
+             */
+            void run();
+
+            /**
              * Method checks whether this algorithm is a source. This means, whether it only has outputs and no inputs.
              *
              * \return true if it only has outputs.
@@ -358,6 +363,14 @@ namespace di
              * \param request if true (default), an update will be requested. Set to false to mark everything done, no update needed anymore.
              */
             void requestUpdate( bool request = true );
+
+            /**
+             * Get notified about changes in a parameter. By default, this method calls \ref requestUpdate, if you override this method, it is your
+             * task to decide whether to update the whole algorithm or not.
+             *
+             * \param parameter the parameter that notified this
+             */
+            virtual void onParameterChange( SPtr< ParameterBase > parameter );
         private:
             /**
              * Algorithm inputs. Fill during construction.

src/lib/di/core/ObserverParameter.cpp
@@ -22,26 +22,28 @@
 //
 //---------------------------------------------------------------------------------------
 
-// A bit crude, but ensures the tag is undefined every time someone includes the logger.
-#ifdef LogTag
-    #undef LogTag
-#endif
+#include <di/core/ParameterBase.h>
 
-#ifndef DI_LOGGER_H
-#define DI_LOGGER_H
+#include "ObserverParameter.h"
 
-#include <iostream>
-#include <iomanip>
+namespace di
+{
+    namespace core
+    {
+        ObserverParameter::ObserverParameter( std::function< void( SPtr< ParameterBase > ) > callback, WPtr< ParameterBase > parameter ):
+            m_callback( callback ),
+            m_parameter( parameter )
+        {
+            // nothing to do.
+        }
 
-// This file contains the preprocessor based logger. This is a rather simple logger, but as it uses a simple ostream interface, it can be replaced
-// later without the need for changes in the code.
-
-#define LogStream std::cout << std::resetiosflags( std::ios_base::basefield | std::ios_base::floatfield | std::ios_base::adjustfield )
-#define LogEnd std::endl;
-#define LogD LogStream << "DEBUG [" << LogTag << "]: "
-#define LogI LogStream << "INFO  [" << LogTag << "]: "
-#define LogW LogStream << "WARN  [" << LogTag << "]: "
-#define LogE LogStream << "ERROR [" << LogTag << "]: "
-#define LogGL LogStream
-
-#endif  // DI_LOGGER_H
+        void ObserverParameter::notify()
+        {
+            auto paramSPtr = m_parameter.lock();
+            if( paramSPtr )
+            {
+                m_callback( paramSPtr );
+            }
+        }
+    }
+}

src/lib/di/core/ObserverParameter.h
@@ -1,0 +1,80 @@
+//---------------------------------------------------------------------------------------
+//
+// Project: DirectionalityIndicator
+//
+// Copyright 2014-2015 Sebastian Eichelbaum (http://www.sebastian-eichelbaum.de)
+//           2014-2015 Max Planck Research Group "Neuroanatomy and Connectivity"
+//
+// This file is part of DirectionalityIndicator.
+//
+// DirectionalityIndicator is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// DirectionalityIndicator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with DirectionalityIndicator. If not, see <http://www.gnu.org/licenses/>.
+//
+//---------------------------------------------------------------------------------------
+
+#ifndef DI_OBSERVERPARAMETER_H
+#define DI_OBSERVERPARAMETER_H
+
+#include <functional>
+
+#include <di/core/ObserverCallback.h>
+#include <di/Types.h>
+
+namespace di
+{
+    namespace core
+    {
+        class ParameterBase;
+
+        /**
+         * Implements a class to monitor changes. It implements the observer pattern and calls a specified function. This specific version forwards
+         * a given parameter to the callback.
+         */
+        class ObserverParameter: public Observer
+        {
+        public:
+            /**
+             * Constructor. Does nothing.
+             *
+             * \param callback the callback that will be called on notify.
+             * \param parameter the parameter to forward to the callback. If the weak_ptr cannot be locked, no callback is called.
+             */
+            ObserverParameter( std::function< void( SPtr< ParameterBase > ) > callback, WPtr< ParameterBase > parameter );
+
+            /**
+             * Destructor. Does nothing.
+             */
+            virtual ~ObserverParameter() = default;
+
+            /**
+             * Notification. The meaning depends on the caller.
+             */
+            void notify() override;
+
+        protected:
+        private:
+            /**
+             * Callback function
+             */
+            std::function< void( SPtr< ParameterBase > ) > m_callback;
+
+            /**
+             * The parameter
+             */
+            WPtr< ParameterBase > m_parameter;
+        };
+    }
+}
+
+#endif  // DI_OBSERVERPARAMETER_H
+

commit 481ec3729119c63d4c272159250237b7ed15206a

fix: ensure that CommandQueue is always locked
unlock() caused a segfault on openSUSE 13.1 / gcc 4.8.1,
introduced in fc0de0d3.

Documentation for std::mutex::unlock says:
  Unlocks the mutex. The mutex must be locked by the current thread of execution,
  otherwise, the behavior is undefined.

Note, actually I could not see why we need unlock at all. Netherveless we keep
that as is.

In general I think there are even more issues here:

  1. The other processCommand() call is running in locked state.
  2. "while( m_running )" could end the loop without going through
     "if( !m_running )"
  3. variables like m_notified and others are used without mutex protection.

src/lib/di/core/CommandQueue.cpp
@@ -109,9 +109,9 @@ namespace di
                     }
 
                     // unlock as this would (otherwise) block the queue itself when "processCommand" is blocking
-                    m_commandQueueMutex.unlock();
-                    // process
+                    lock.unlock();
                     processCommand( command );
+                    lock.lock();
                 }
             }
         }